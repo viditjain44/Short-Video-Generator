2345a30487211e7d11be350c09276ee69a260437
vidit jain <jainvidit365vidit@gmail.com>
2024-12-24 05:15:22 +0530

    db integration and auth

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
     <h2>Success cannot be fatal</h2> 
     <Button variant="secondary">PRESS if u agree</Button>
     
-    
+    console.log(process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY)
     </div>
   );
 }
---
+++ app/provider.js
@@ -0,0 +1,13 @@
+import React from "react";
+function Provider(children) {
+    return ( 
+
+
+        <div>
+            
+{children}
+
+        </div>
+    )
+}
+export default Provider;
